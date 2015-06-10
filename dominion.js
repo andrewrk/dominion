e05a7b232a77997683193f5396451ece93319800
Andrew Kelley <superjoe30@gmail.com>
2015-06-10 12:16:24 -0700

    deal cards to players

--- dominion.js
+++ dominion.js
@@ -1,16 +1,34 @@
 var dominion = importAndProcessCards();
+var nextState = 0;
+var STATE_ACTION = nextState++;
+var STATE_BUY = nextState++;
 
 var state = shuffleAndDeal(2);
+console.log(enumerateMoves(state));
 printGameState(state);
 
+function enumerateMoves(state) {
+}
+
 function shuffleAndDeal(howManyPlayers) {
+  var i;
+  var players = [];
+  for (i = 0; i < howManyPlayers; i += 1) {
+    players.push(createPlayerState(i));
+  }
   var state = {
+    currentPlayerIndex: 0,
+    state: STATE_BUY,
+    actionCount: 0,
+    buyCount: 1,
+    treasureCount: 0,
     cardList: [],
     cardTable: {},
+    players: players,
   };
 
   var listOfCardsPerSet = {};
-  var list, card, i;
+  var list, card;
   for (i = 0; i < dominion.cardList.length; i += 1) {
     card = dominion.cardList[i];
     if (!card.set) continue;
@@ -54,13 +72,55 @@ function shuffleAndDeal(howManyPlayers) {
     state.cardTable[card.name] = gameCard;
     state.cardList.push(gameCard);
   }
+
+  function createPlayerState(playerIndex) {
+    var estateCard = getCard('Estate');
+    var copperCard = getCard('Copper');
+    var deck = [];
+    var hand = [];
+    var i;
+    for (i = 0; i < 7; i += 1) {
+      deck.push(copperCard);
+    }
+    for (; i < 10; i += 1) {
+      deck.push(estateCard);
+    }
+    shuffleArray(deck);
+    for (i = 0; i < 5; i += 1) {
+      hand.push(deck.pop());
+    }
+    return {
+      index: playerIndex,
+      deck: deck,
+      hand: hand,
+    };
+  }
 }
 
 function printGameState(state) {
-  for (var i = 0; i < state.cardList.length; i += 1) {
+  var i;
+  for (i = 0; i < state.cardList.length; i += 1) {
     var card = state.cardList[i];
-    console.log(card.card.name + " x" + card.count);
+    console.log("(" + card.card.cost + ") " + card.card.name + " x" + card.count);
   }
+  for (i = 0; i < state.players.length; i += 1) {
+    printPlayerState(state.players[i]);
+  }
+
+  function printPlayerState(player) {
+    console.log("Player " + (player.index + 1) + ":");
+    console.log("  deck: " + deckToString(player.deck));
+    console.log("  hand: " + deckToString(player.hand));
+  }
+}
+
+function deckToString(deck) {
+  if (deck.length === 0) return "(empty)";
+  return deck.map(getCardName).join(" ");
+}
+
+function getCardName(card) {
+  return card.name;
 }
 
 function importAndProcessCards() {
@@ -113,4 +173,20 @@ function compare(a, b){
   } else {
     return 1;
   }
+}
+
+function shuffleArray(list) {
+  var counter = list.length;
+  while (counter) {
+    var index = Math.floor(Math.random() * counter--);
+    var temp = list[counter];
+    list[counter] = list[index];
+    list[index] = temp;
+  }
+}
+
+function getCard(name) {
+  var card = dominion.cardTable[name];
+  if (!card) throw new Error("card not found: " + name);
+  return card;
 }
